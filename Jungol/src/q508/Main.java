30d682e32848695493ab4f57a2d643e6142b7aa3
21-manic-016g <21-manic-016g@21-manic-016>
2024-03-21 17:39:52 +0900

    Clear

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,14 @@
+package q508;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" subject score");
+		System.out.println("==============");
+		System.out.println("  korean    90");
+		System.out.println(" english   100");
+		System.out.println("computer    80");
+	}
+
+}
