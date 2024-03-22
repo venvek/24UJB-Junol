040c77e562469cd0d1ccc129554c9a9594c001e3
21-manic-016g <21-manic-016g@21-manic-016>
2024-03-22 17:38:35 +0900

    c

--- Jungol/src/q506/Main.java
+++ Jungol/src/q506/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int var1 = 170;
+		double var2 = 68.6;
+		System.out.println("My height");
+		System.out.println(var1);
+		System.out.println("My weight");
+		System.out.printf("%.6f",68.6);
 	}
 
 }
