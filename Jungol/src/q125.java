9be2916be9d86c325b48ccd18a5ce03d224987f4
21-manic-016g <21-manic-016g@21-manic-016>
2024-03-27 16:39:55 +0900

    c

---
+++ Jungol/src/q125.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class q125 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int j =1;
+		while(j<=a) {
+			System.out.print(j + " ");
+			j++;
+		}
+		sc.close();
+		
+		//1. for loop
+		//for(int j=1;i<=a;i++)
+	}
+}
