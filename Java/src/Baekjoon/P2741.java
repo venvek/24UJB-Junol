a1b154a1eea7a99b90322c257eb972b44458bbda
dhrwn <dhrwn@DESKTOP-TSOSNJ9>
2024-04-08 00:47:39 +0900

    cc

---
+++ Java/src/Baekjoon/P2741.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class P2741 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		sc.close();
+		
+		System.out.println(N);
+	}
+}
